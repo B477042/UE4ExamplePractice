0e24ef2c61c82b85762716a32a263997e0ae3328
B477042 <37619106+B477042@users.noreply.github.com>
2019-08-13 16:27:43 +0900

    2019 08 13 chapter 13 -1
    study ini file

---
+++ ArenaBattle/Source/ArenaBattleSetting/Public/ABCharacterSetting.h
@@ -0,0 +1,25 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/NoExportTypes.h"
+#include "ABCharacterSetting.generated.h"
+
+/**
+ *	This Class is Manage Character Assetes. By Using INI Files. 
+	
+ */
+//To Use INI File, add 'config' and file name
+//in this case, Use  ArenaBattle.ini 
+UCLASS(config=ArenaBattle)
+class ARENABATTLESETTING_API UABCharacterSetting : public UObject
+{
+	GENERATED_BODY()
+public:
+	UABCharacterSetting();
+	UPROPERTY(config)
+		FSoftObjectPath CharacterAssets;
+	UPROPERTY(config)
+
+};
